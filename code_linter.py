eed5a370bb163e3a26f00556db0895bfaeff3fde
GzzDany <60372312+GzzDany@users.noreply.github.com>
2024-03-12 19:56:25 -0600

    Update code_linter.py

--- code_linter.py
+++ code_linter.py
@@ -4,6 +4,5 @@ import inspect
 def count_comparisons(func):
     tree = ast.parse(inspect.getsource(func))
     boolean_count = sum([1 for node in ast.walk(tree) if isinstance(node, ast.Compare)])
-    if boolean_count > 2:
-        raise AssertionError: "Your function uses more than two comparison statements! In order to get full points, you should ensure you're properly using dictionaries!" 
+    assert boolean_count <= 2, "Your function uses more than two comparison statements! In order to get full points, you should ensure you're properly using dictionaries!" 
     return 
